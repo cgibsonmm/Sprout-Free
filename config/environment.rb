c23253bdfe951051b2f9a28b9b64d8331c906f79
cgibsonmm <cgibsonmm@gmail.com>
2018-10-31 15:32:36 -0400

    pass change

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@ Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
   :user_name => Rails.application.credentials.dig(:send_grid, :user_name),
-  :password => Rails.application.credentials.dig(:send_grid, :user_name),
+  :password => Rails.application.credentials.dig(:send_grid, :password),
   :domain => 'sprout-free-canada.herokuapp.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
