b342c4eb20d0ef05fba7e3e48fe7218b25a8a762
cgibsonmm <cgibsonmm@gmail.com>
2018-11-09 17:52:07 -0500

    admin can drag

--- app/assets/javascripts/forum_sortable.js
+++ app/assets/javascripts/forum_sortable.js
@@ -1,5 +1,6 @@
 $(document).on("turbolinks:load", function(){
   if (gon.admin){
+    console.log('Admin can drag');
     $('#forum_categories').sortable({
       update: function(e, ui){
         Rails.ajax({
@@ -9,5 +10,15 @@ $(document).on("turbolinks:load", function(){
         })
       }
     });
+
+    $('#forum_topics').sortable({
+      update: function(e, ui){
+        Rails.ajax({
+          url: $(this).data('url'),
+          type: "PATCH",
+          data: $(this).sortable('serialize')
+        })
+      }
+    })
   };
 });
