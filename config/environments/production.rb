253309f7a14805969445ed484fae2f69122e1002
cgibsonmm <cgibsonmm@gmail.com>
2018-11-19 11:29:30 -0500

    added test views to error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,9 @@ Rails.application.configure do
 
   config.force_ssl = true
 
+
+  config.exceptions_app = self.routes
+
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
