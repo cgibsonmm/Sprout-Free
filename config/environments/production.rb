2cad9106985e9eed38fb5a8589e3f6836bca6187
cgibsonmm <cgibsonmm@gmail.com>
2018-12-05 12:36:37 -0500

    testing protocol

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { :host => 'https://www.sprout-free-canada.herokuapp.com/' }
+  config.action_mailer.default_url_options = {:protocol => 'https', :host => 'sprout-free-canada.herokuapp.com/' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
