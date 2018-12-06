2e53d4830469a6d2661312dfc225704fa71750c1
cgibsonmm <cgibsonmm@gmail.com>
2018-12-06 13:07:32 -0500

    added button for sorting to work

--- app/assets/javascripts/forum_sortable.js
+++ app/assets/javascripts/forum_sortable.js
@@ -1,27 +1,27 @@
-$(document).on('turbolinks:load', function(){
-  console.log('checking drag')
-  setTimeout(function(){
-    if(gon.admin == true){
-      console.log('admin can drag')
-      $('#forum_categories').sortable({
-        update: function(e, ui){
-          Rails.ajax({
-            url: $(this).data('url'),
-            type: "PATCH",
-            data: $(this).sortable('serialize')
-          });
-        }
+runSort = function(){
+  $('#forum_categories').sortable({
+    update: function(e, ui){
+      Rails.ajax({
+        url: $(this).data('url'),
+        type: "PATCH",
+        data: $(this).sortable('serialize')
       });
+    }
+  });
 
-      $('#forum_topics').sortable({
-        update: function(e, ui){
-          Rails.ajax({
-            url: $(this).data('url'),
-            type: "PATCH",
-            data: $(this).sortable('serialize')
-          });
-        }
+  $('#forum_topics').sortable({
+    update: function(e, ui){
+      Rails.ajax({
+        url: $(this).data('url'),
+        type: "PATCH",
+        data: $(this).sortable('serialize')
       });
-    };
-  }),5000
+    }
+  });
+}
+
+
+$(document).on('turbolinks:load', function(){
+  sortBtn = $('#sort-btn')
+  sortBtn.on('click', runSort)
 });
--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -1,15 +1,14 @@
 RSpec.configure do |config|
-
   config.before(:suite) do
-    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.clean_with(:deletion)
   end
 
   config.before(:each) do
     DatabaseCleaner.strategy = :transaction
   end
 
-  config.before(:each, :js => true) do
-    DatabaseCleaner.strategy = :truncation
+  config.before(:each, js: true) do
+    DatabaseCleaner.strategy = :deletion
   end
 
   config.before(:each) do
