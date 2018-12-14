2b8ffbf2de29fee8e8ad2af05e97d03c9ff231c5
cgibsonmm <cgibsonmm@gmail.com>
2018-12-14 10:21:08 -0500

    dropped unworking at.js trix fix

--- app/assets/javascripts/trix_at.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("trix-initialize", function(event) {
-  var element = $("trix-editor");
-  element.on("inserted.atwho", function(event, flag, query) {
-      element[0].editor.insertString(flag[0].innerHTML);
-  });
-});
