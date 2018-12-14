9aa8c1d9f4fc326f87188496ff7d929d6dfe33b1
cgibsonmm <cgibsonmm@gmail.com>
2018-12-13 22:58:41 -0500

    changed log levels

--- app/models/forum_post.rb
+++ app/models/forum_post.rb
@@ -22,13 +22,8 @@ class ForumPost < ApplicationRecord
 
   def notifiy_users
     mentioned_users.each do |user|
-      puts '!!!!!!!!HELLO'
-      puts ''
-      puts ''
-      puts ''
       notification = Notification.create(recipient: user, actor: self.user, action: 'mentioned', notifiable: self)
       notification.save
-      puts '________________'
     end
   end
 
