e11b582d960fe7508a8097026e362c422367f219
cgibsonmm <cgibsonmm@gmail.com>
2019-01-16 21:18:08 -0500

    finished basic layout for navbar

--- app/assets/javascripts/materialize.js
+++ app/assets/javascripts/materialize.js
@@ -1,3 +1,7 @@
 $(document).on('turbolinks:load', function(){
   $('.dropdown-trigger').dropdown({coverTrigger: false,});
+  $('.dropdown-right-trigger').dropdown({
+    coverTrigger: false,
+    alignment: 'right'
+  })
 })
