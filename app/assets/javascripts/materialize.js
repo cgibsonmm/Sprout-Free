d93e9b2cee351e136dde7f1fa93f931df9340103
cgibsonmm <cgibsonmm@gmail.com>
2019-01-17 18:32:36 -0500

    basic placholder homepage

--- app/assets/javascripts/materialize.js
+++ app/assets/javascripts/materialize.js
@@ -1,7 +1,16 @@
 $(document).on('turbolinks:load', function(){
+  $('.materialboxed').materialbox();
   $('.dropdown-trigger').dropdown({coverTrigger: false,});
   $('.dropdown-right-trigger').dropdown({
     coverTrigger: false,
     alignment: 'right'
   })
+  $('.carousel').carousel({
+    fullWidth: true,
+    duration: 500,
+  });
+
+  setInterval(function() {
+      $('.carousel').carousel('next');
+    }, 5000);
 })
