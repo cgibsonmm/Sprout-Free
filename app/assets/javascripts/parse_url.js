d7e0de8bff4e8da2a1170d9c4705c56cbf135708
cgibsonmm <cgibsonmm@gmail.com>
2019-02-01 08:06:02 -0500

    disabling image url parser

--- app/assets/javascripts/parse_url.js
+++ app/assets/javascripts/parse_url.js
@@ -1,16 +1,16 @@
-$(document).on('turbolinks:load', function(){
-  $('a[href^="https://sprout-free.s3"]').each(function(){
-    var oldUrl = $(this).attr("href")
-    var newulr = oldUrl.replace(oldUrl, oldUrl.split('?')[0])
-
-    $(this).attr('href', newulr)
-
-    $(this).each(function(){
-      var link = $(this).find('img')
-      var old_src = link.attr('src')
-      var newulr = old_src.replace(old_src, old_src.split('?')[0])
-
-      $(link).attr('src', newulr)
-    })
-  });
-});
+// $(document).on('turbolinks:load', function(){
+//   $('a[href^="https://sprout-free.s3"]').each(function(){
+//     var oldUrl = $(this).attr("href")
+//     var newulr = oldUrl.replace(oldUrl, oldUrl.split('?')[0])
+//
+//     $(this).attr('href', newulr)
+//
+//     $(this).each(function(){
+//       var link = $(this).find('img')
+//       var old_src = link.attr('src')
+//       var newulr = old_src.replace(old_src, old_src.split('?')[0])
+//
+//       $(link).attr('src', newulr)
+//     })
+//   });
+// });
