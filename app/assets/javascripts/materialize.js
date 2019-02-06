e9635f917d1964e996fe548cdcd76621ee683636
cgibsonmm <cgibsonmm@gmail.com>
2019-02-06 08:46:28 -0500

    added changes to navs and responsive

--- app/assets/javascripts/materialize.js
+++ app/assets/javascripts/materialize.js
@@ -1,6 +1,10 @@
 $(document).on('turbolinks:load', function(){
   $('.collapsible').collapsible();
   $('.materialboxed').materialbox();
+  $('.categories-dropdown-trigger').dropdown({
+    coverTrigger: false,
+    constrainWidth: false,
+  })
   $('.dropdown-trigger').dropdown({coverTrigger: false,});
   $('.dropdown-right-trigger').dropdown({
     coverTrigger: false,
