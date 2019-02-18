5cb8c52c5064191621e172771356b22c5c111a76
cgibsonmm <cgibsonmm@gmail.com>
2019-02-17 19:16:48 -0500

    added top thread to home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,13 @@
 class HomeController < ApplicationController
+  before_action :top_forum_thread
+
   def index
     @latest_forum_thread = ForumThread.last
+  end
+
+  private
+
+  def top_forum_thread
+    @top_thread = ForumThread.all.max_by { |thread| thread.forum_posts.count }
   end
 end
